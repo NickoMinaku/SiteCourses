4aff3a82afa49931b3d8672eea824854145872de
NickoMinaku <trueshchev@mail.ru>
2023-02-21 01:42:07 +0500

    Add favicon, add courses page, some changes for page titles, some fixes

--- SiteCourses/Controllers/HomeController.cs
+++ SiteCourses/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SiteCourses.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Courses() 
         {
             return View();
         }
